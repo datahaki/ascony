d2b7ec61cbb4dfdd4dac874effa207d5c9813fdc
datahaki <jan.hakenberg@gmail.com>
2026-03-01 18:53:19 +0100

    sanity check ascony

--- src/main/java/ch/alpine/ascony/win/SanityCheckAscony.java
+++ src/main/java/ch/alpine/ascony/win/SanityCheckAscony.java
@@ -3,26 +3,27 @@ package ch.alpine.ascony.win;
 
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.util.function.Consumer;
 
 import ch.alpine.ascony.dis.ManifoldDisplays;
+import ch.alpine.bridge.pro.SanityCheckRunProvider;
+import ch.alpine.bridge.pro.WindowProvider;
 import ch.alpine.tensor.Throw;
 
 /** DO NOT USE IN THE APPLICATION LAYER */
-public enum SanityCheckAbstractDemo implements Consumer<AbstractDemo> {
-  INSTANCE;
-
+public class SanityCheckAscony extends SanityCheckRunProvider {
   private static final int WIDTH = 800;
   private static final int HEIGHT = 800;
 
   @Override
-  public void accept(AbstractDemo abstractDemo) {
-    if (abstractDemo instanceof ManifoldDisplayDemo manifoldDisplayDemo) {
+  protected void check(WindowProvider windowProvider) {
+    if (windowProvider instanceof ManifoldDisplayDemo manifoldDisplayDemo)
       check(manifoldDisplayDemo);
-    }
+    else
+      super.check(windowProvider);
   }
 
   private void check(ManifoldDisplayDemo manifoldDisplayDemo) {
+    IO.println("CORRECT ENTER");
     BufferedImage bufferedImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
     Graphics2D graphics = bufferedImage.createGraphics();
     manifoldDisplayDemo.timerFrame.jFrame.setSize(WIDTH, HEIGHT);
