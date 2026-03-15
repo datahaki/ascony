6f6d5be790030812f80f37ea63d88470de72278a
datahaki <jan.hakenberg@gmail.com>
2026-03-15 11:30:17 +0100

    minor

--- src/main/java/ch/alpine/ascony/ren/ColorStroke.java
+++ src/main/java/ch/alpine/ascony/ren/ColorStroke.java
@@ -10,4 +10,7 @@ public record ColorStroke(Color color, Stroke stroke) implements Serializable {
   public static final ColorStroke CURVE = new ColorStroke(Color.BLUE, new BasicStroke(1.25f));
   public static final ColorStroke SECONDARY_CURVE = new ColorStroke(Color.RED, new BasicStroke());
   public static final ColorStroke CURVATURE_COMB = new ColorStroke(new Color(0, 192, 0, 128), new BasicStroke());
+  private static final Stroke STROKE_GEODESIC = //
+      new BasicStroke(2f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] { 3 }, 0);
+  public static final ColorStroke AREA_SELECTION = new ColorStroke(Color.LIGHT_GRAY, STROKE_GEODESIC);
 }
