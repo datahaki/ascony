5482f7f3f4d26440840fffa93bc5bfd7e5b2476b
datahaki <jan.hakenberg@gmail.com>
2026-03-25 07:32:26 +0100

    minor

--- src/main/java/ch/alpine/ascony/ren/ColorStroke.java
+++ src/main/java/ch/alpine/ascony/ren/ColorStroke.java
@@ -13,6 +13,8 @@ public record ColorStroke(Color color, Stroke stroke) implements Serializable {
   private static final ColorDataIndexed COLOR_DATA_INDEXED = ColorDataLists._097.strict();
   private static final Stroke STROKE_GEODESIC = //
       new BasicStroke(2f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] { 3 }, 0);
+  private static final Stroke STROKE_BOLD = //
+      new BasicStroke(4f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] { 3 }, 0);
   // ---
   public static final ColorStroke CURVE = new ColorStroke(Color.BLUE, new BasicStroke(1.5f));
   public static final ColorStroke SECONDARY_CURVE = new ColorStroke(Color.RED, new BasicStroke());
@@ -20,4 +22,5 @@ public record ColorStroke(Color color, Stroke stroke) implements Serializable {
   public static final ColorStroke TRACE = new ColorStroke(Color.LIGHT_GRAY, STROKE_GEODESIC);
   public static final ColorStroke CONVEX_HULL = new ColorStroke(COLOR_DATA_INDEXED.getColor(1), STROKE_GEODESIC);
   public static final ColorStroke SPHERE_1 = new ColorStroke(new Color(192, 192, 192, 128), new BasicStroke());
+  public static final ColorStroke HI_CONTRAST = new ColorStroke(new Color(255, 0, 255), STROKE_BOLD);
 }
